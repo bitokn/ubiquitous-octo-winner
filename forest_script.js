766f96deb60399b622de2bac2409355802021c18
bernabe <b.kondrak45@gmail.com>
2024-05-28 17:36:48 -0600

    reformatted main page

--- forest_script.js
+++ forest_script.js
@@ -1,11 +1,10 @@
 function doFunction1() {
-    document.getElementById('textbox1').innerHTML = "press the button to grimace";
+    let grimaceField = document.getElementById('textbox1')
+    grimaceField.innerHTML = "press button to grimace"
+    grimaceField.style.fontSize = "48px"
 }
 doFunction1()
 function doFunction2() {
-    document.getElementById('textbox1').style.fontSize = "48px";
-    document.getElementById('textbox1').rows = "1";
-    document.getElementById('textbox1').cols = "5";
     document.getElementById('textbox1').innerHTML = "&#128556;&#128556;&#128556;";
     // var x = document.createElement("P");
     // var t = document.createTextNode("&#128556;");
