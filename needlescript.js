124ed66f13d82eee9be538e358ee93b45542d1fb
bito <b.kondrak45@gmail.com>
2024-05-29 23:13:55 -0600

    instead of textarea emojis just get created in p element

--- needlescript.js
+++ needlescript.js
@@ -1,10 +1,10 @@
 createButton = function (_value, area) {
     const new_button = document.createElement("button")
     new_button.innerHTML = _value
+    new_button.id = "button_" + _value[7]
+    // console.log(new_button.id)
     new_button.onclick = function () {
-        let bruhNode = document.createElement("p")
-        bruhNode.innerHTML = _value
-        area.appendChild(bruhNode)
+        emojiField.innerHTML += _value
     }
     return new_button
 }
@@ -13,14 +13,17 @@ createButton = function (_value, area) {
 window.onload = function () {
     const emojiArea = document.getElementById("emojiArea")
     const buttonArea = document.getElementById("buttonArea")
-
+    const emojiField = document.getElementById("emojiField")
     for (let i = 0; i < 9; i++) {
         let emojiValue = "&#12851" + i + ";"
         if ((i % 3) == 0) {
             buttonArea.appendChild(document.createElement("br"))
         }
 
-        buttonArea.appendChild(createButton(`${emojiValue}`, emojiArea))
+        buttonArea.appendChild(createButton(`${emojiValue}`, emojiField))
 
     }
+    // for (let i = 0; i < 10000; i++) {
+    //     button_1.onclick()
+    // }
 }
